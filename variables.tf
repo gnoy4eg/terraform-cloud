6d4339e24ca2d9e722be3095ae9dd9d72d1bb299
Sergey Chipyshev <pocjta1943@gmail.com>
2022-04-04 13:01:51 +0500

    edit project

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "yandex_cloud_id" {
+  default = "b1gffcps5oa5h9clc5o9"
+}
+
+variable "yandex_folder_id" {
+  default = "b1gj0vgiglqro7mntfrm"
+}
+
+variable "centos-7-base" {
+  default = "fd80905qm2ckngipg7ip"
+}
